38f73475458635b6630e664ba7e1e829c03ea61e
IvanHrIvanov <vinko96@abv.bg>
2020-11-02 01:38:32 +0200

    Add

---
+++ 02. Shopping List/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Shopping_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> shopingList = Console.ReadLine().Split("!").ToList();
+
+            string[] comand = Console.ReadLine().Split(" ").ToArray();
+
+            while (comand[0] != "Go")
+            {
+
+                if (comand[0] == "Urgent")
+                {
+                    if (!shopingList.Contains(comand[1]))
+                    {
+                        shopingList.Insert(0, comand[1]);
+                    }
+                   
+                }
+
+                else if (comand[0] == "Unnecessary" && shopingList.Contains(comand[1]))
+                {
+                    shopingList.Remove(comand[1]);
+                }
+
+                else if (comand[0] == "Correct" && shopingList.Contains(comand[1]))
+                {
+                    string oldItem = comand[1];
+                    string newItem = comand[2];
+                    int index = 0;
+                    foreach (var item in shopingList)
+                    {
+                        index++;
+                        if (item == oldItem)
+                        {
+                            shopingList.Insert(index, newItem);
+                            break;
+                        }
+                    }
+                    
+                    shopingList.Remove(oldItem);
+                    
+                }
+                else if(comand[0]=="Rearrange")
+                {
+                    shopingList.Insert(shopingList.Count, comand[1]);
+                    shopingList.Remove(comand[1]);
+                }
+
+                comand = Console.ReadLine().Split(" ").ToArray();
+            }
+            Console.WriteLine(string.Join(", ",shopingList));
+        }
+    }
+}
---
+++ 03. Heart Delivery/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Heart_Delivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] neighborhood = Console.ReadLine().Split("@").Select(int.Parse).ToArray();
+
+            string[] comand = Console.ReadLine().Split(" ");
+            int decreased = 2;
+            int house = 0;
+            while (comand[0] != "Love!")
+            {
+                int jump = int.Parse(comand[1]);
+
+                for (int i = 0; i < jump; i++)
+                {
+                    house += 1;                   
+                }
+
+                if (house > neighborhood.Length-1)
+                {
+                    house = 0;
+                }
+
+                if (neighborhood[house] <= 0)
+                {
+                    Console.WriteLine($"Place {house} already had Valentine's day.");
+                }
+                else
+                {
+                    neighborhood[house] -= decreased;
+
+                    if (neighborhood[house] <= 0)
+                    {
+                        Console.WriteLine($"Place {house} has Valentine's day.");
+                    }
+                }
+
+                comand = Console.ReadLine().Split(" ");
+            }
+            int failedHouses = neighborhood.Count(x => x > 0);
+
+            Console.WriteLine($"Cupid's last position was {house}.");
+
+            if (failedHouses > 0)
+            {
+                Console.WriteLine($"Cupid has failed {failedHouses} places.");
+            }
+            else
+            {
+                Console.WriteLine("Mission was successful.");
+            }
+        }
+    }
+}
